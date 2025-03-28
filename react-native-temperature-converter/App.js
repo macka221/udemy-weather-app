942fdd211418a09115bdec5636b7a12b502fb159
Zach <zachare18@gmail.com>
2025-03-28 15:04:15 -0400

    Setting up background and input field text

--- react-native-temperature-converter/App.js
+++ react-native-temperature-converter/App.js
@@ -1,14 +1,24 @@
-import { StyleSheet, Text } from 'react-native';
-import { mainStyles } from './app.style';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { useState } from 'react';
+import { mainStyles } from './app.style';
+import { Input } from './components/input/input';
+import hotBackground from './assets/hot.png';
 
 export default function App() {
+  const [inputValue, setInputValue] = useState(0);
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={ mainStyles.root }>
-        <Text>Hello</Text>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <ImageBackground style={ mainStyles.backgroundImage } source={ hotBackground }>
+      <SafeAreaProvider>
+        <SafeAreaView style={ mainStyles.root }>
+          <View style={ mainStyles.workspace }>
+            <Text>{inputValue}</Text>
+            <Input onChange={setInputValue} defaultValue={ 0 } />
+            <Text>Button</Text>
+          </View>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </ImageBackground>
  );
 }
 
